6ecbd3fb3ca80b433f7bead12637c2db78852194
Mohammad Javad <mohammadjavada78@gmail.com>
2025-05-25 10:05:34 +0330

    fix error for sw.js not found in vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,25 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      manifest: false,
+      filename: "sw.js",
+    }),
+    reactRouter(), // Keep this last to ensure proper ordering
+  ],
+  server: {
+    headers: {
+      "Service-Worker-Allowed": "/",
+    },
+  },
 });
